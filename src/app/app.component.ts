36ca1bd514793305652701650ea58f581d914b2d
Cathbridge <catmlangeni@gmail.com>
2018-06-15 09:17:35 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InfoService } from './services/info.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  profile ;
+
+   constructor(private info: InfoService){
+    this.info.getUser().subscribe(profile => {this.profile = profile});
+
+    //Another subscribe exp
+//  this.info.getInfo().subscribe(res => { this.profile = res})
+  }
 }
